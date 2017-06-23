e2cfff3de54a1bbb25907520dd20a933009fe7df
JohnVonNeumann <louiswillcock@gmail.com>
2017-06-23 23:51:51 +1000

    some more changes, pulled config.py out of gitignore as it will not be holding any secrets in it, will be using envs to pass in anything senstive, and keep a more secure workflow

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQL_ALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+class ProductionConfig(Config):
+    PRODUCTION = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = True
+    DEVELOPMENT = True
