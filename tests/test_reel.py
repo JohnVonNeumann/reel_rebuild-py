9b67fb8c54fa425172c9dc34ac33e6b4e62bbe4f
JohnVonNeumann <louiswillcock@gmail.com>
2017-06-25 16:07:31 +1000

    added basic unittest functionality, just barebones from an online blog, will be kitting it out now.

--- tests/test_reel.py
+++ tests/test_reel.py
@@ -1,1 +1,25 @@
 # Test Suite
+import unittest
+
+class MyTestClass(unittest.TestCase):
+
+    @classmethod
+    def setupClass(cls):
+        pass
+    
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_equal_numbers(self):
+        self.assertEqual(2, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
