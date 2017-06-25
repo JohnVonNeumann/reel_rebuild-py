00031ff181ab568be88d6fdcfb2ae355015b8db0
JohnVonNeumann <louiswillcock@gmail.com>
2017-06-26 08:33:03 +1000

    small change to start using flask-sqlalchemy, used my commute to work  out the differences between sqlalhemy and flask-sqlalchemy

--- reel/__init__.py
+++ reel/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import reel.views
+
+ 
 app = Flask(__name__) 
+db = SQLAlchemy(app)
 app.config.from_pyfile('/home/xibalba/code/reel/config.py') 
  
-import reel.views
 
