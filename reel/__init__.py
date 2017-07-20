522f5981c62308ab7cfa60906ab96e394ffb9522
JohnVonNeumann <louiswillcock@gmail.com>
2017-07-20 21:16:14 +1000

    and anther crack at a clear test run of travis ci, made some clean edits, glad to finally have learnt how (a tiny fraction) of the os module works

--- reel/__init__.py
+++ reel/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
-app = Flask(__name__) 
+app = Flask(__name__)
 
 import reel.views
-app.config.from_pyfile('/home/xibalba/code/reel/config.py') 
- 
+app.config.from_pyfile(os.path.abspath('config.py')
 
