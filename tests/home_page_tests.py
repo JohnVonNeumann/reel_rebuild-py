ede07229823806a9a315dc538c4aafb68a616eca
JohnVonNeumann <louiswillcock@gmail.com>
2017-07-22 13:30:48 +1000

    cleaned up the home_page_test file, removed generic things and items that indicated that tests were running with too much understanding of implementation details

--- tests/home_page_tests.py
+++ tests/home_page_tests.py
@@ -1,35 +1,22 @@
 # Test Suite
 import unittest
 from reel import app
-from reel.views import home_welcome
 
 
-class HomePageTesttClass(unittest.TestCase):
-
-    @classmethod
-    def setupClass(cls):
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
+class HomePageTestClass(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
+    def test_homepage_response(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"Welcome to Reel!", result.data)
+
     def tearDown(self):
         pass
 
-    def test_home_status_code(self):
-        result = self.app.get('/')
-
-        self.assertEqual(result.status_code, 200)
-
-    def test_home_welcome_return(self):
-        response = self.app.get('/')
-        self.assertIn(b"Welcome to Reel!", response.data)
-
-
 if __name__ == '__main__':
     unittest.main()
+    print(test_homepage_response.result)
